c4aad91fb7abdebc58d71e3cc68d56c85e1c2cab
Anh Dinh <anhdinh17@gmail.com>
2024-05-20 22:03:03 -0500

    Add Generic networking function

--- Combine Networking/NetworkingManager.swift
+++ Combine Networking/NetworkingManager.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import Combine
+
+class NetworkingManager {
+    static let shared = NetworkingManager()
+    private init() {}
+    
+    func fetchURL<T: Codable>(_ url: URL) -> AnyPublisher<T, Error> {
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap({ result in
+                let decoder = JSONDecoder()
+                return try decoder.decode(T.self, from: result.data)
+            })
+            .eraseToAnyPublisher()
+    }
+}
