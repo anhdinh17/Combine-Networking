09ca7da149745f9973b562cced3adbe3b38e6901
Anh Dinh <anhdinh17@gmail.com>
2024-05-21 23:12:25 -0500

    Create Netwoking func by using Combine

--- Combine Networking/NetworkingManager.swift
+++ Combine Networking/NetworkingManager.swift
@@ -8,10 +8,43 @@
 import Foundation
 import Combine
 
+struct User: Codable {
+    let id: Int
+    let name: String
+    let username: String
+}
+
+/** ---NOTE---
+ - In the book, he creates a struct for Error.
+ - I think it's a bit complicated so I just create an enum to do error handling.
+ */
+
+enum NetworkingError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unknown
+    
+    var errorDescription: String {
+        switch self {
+        case .invalidURL:
+            return "This is invalid URL error."
+        case .invalidResponse:
+            return "This is invalid RESPONSE ERROR."
+        case .invalidData:
+            return "This is INVALID DATA ERROR."
+        case .unknown:
+            return "Sorry, we don't know this error lol."
+        }
+    }
+}
+
 class NetworkingManager {
     static let shared = NetworkingManager()
     private init() {}
-    
+
+    // This func returns a PUBLISHER
+    // In this case, it's dataTaskPublisher
     func fetchURL<T: Codable>(_ url: URL) -> AnyPublisher<T, Error> {
         URLSession.shared.dataTaskPublisher(for: url)
             .tryMap({ result in
@@ -19,5 +52,22 @@ class NetworkingManager {
                 return try decoder.decode(T.self, from: result.data)
             })
             .eraseToAnyPublisher()
+    }
+    
+    // ANOTHER FUNC
+    // This way is easier to understand than .flatMap{}
+    func fetchURLWithErrorHandling<T: Decodable>(_ url: URL) -> AnyPublisher<[T], Error> {
+      URLSession.shared.dataTaskPublisher(for: url)
+        .tryMap({ result in
+          let decoder = JSONDecoder()
+          guard let urlResponse = result.response as? HTTPURLResponse, (200...299).contains(urlResponse.statusCode) else {
+              // Errors occur while decoding in tryMap are AUTOMATICALLY
+              // forwarded to subscriber(.sink)
+              throw NetworkingError.invalidResponse
+          }
+
+          return try decoder.decode([T].self, from: result.data)
+        })
+        .eraseToAnyPublisher()
     }
 }
--- Combine Networking/ViewController.swift
+++ Combine Networking/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
-
+    
+    var networkPublisher: AnyPublisher<[User], Error> = NetworkingManager.shared.fetchURLWithErrorHandling(URL(string: "https://jsonplaceholder.typicode.com/usersss")!)
+    
+    var cancellables = Set<AnyCancellable>()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Call function
+        // Which trigger value change
+        networkPublisher
+            .sink { completionStatus in
+                switch completionStatus {
+                case .finished:
+                    print("Finished Publishing")
+                case .failure(let error):
+                    if error as! NetworkingError == NetworkingError.invalidResponse {
+                        print("Error of Invalid Response")
+                    }
+                }
+            } receiveValue: { usersArray in
+                print(usersArray)
+            }
+            .store(in: &cancellables)
+
     }
-
-
 }
 
